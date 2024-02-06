efbcd8d5ff2a0942628026309b81737fedeb8d3e
Илья <149475177+Kusok0vody@users.noreply.github.com>
2024-02-06 22:55:39 +0700

    small changes
    1. change quivers' formation
    2. better comments
    3. arrays before functions are deleted (now only functions)

--- visualization.py
+++ visualization.py
@@ -2,99 +2,91 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# The whole program will be just functions later
 
-# Importing data
-# u_path = input("path to file with  data: ")
-# data_u = np.load(u_path)
 
-# Vx_path = input("path to file with velocity data: ")
-# data_Vx = np.load(Vx_path)
-
-# Vy_path = input("path to file with velocity data: ")
-# data_Vy = np.load(Vy_path)
-
-# Random values (temporally)
-data_u = []
-data_Vx = []
-data_Vy = []
-
-for t in range(0,120,1):
-    # u = [[np.sin(i/5) for j in range(0,100)] for k in range(0,100)]
-    u = np.random.uniform(-10,10,(100,100))
-    vx = np.cos(t/10)*np.ones((100,100))
-    vy = np.sin(t/10)*np.ones((100,100))
-    data_u.append(u)
-    data_Vx.append(vx)
-    data_Vy.append(vy)
-
-data_u = np.array(data_u)
-data_Vx = np.array(data_Vx)
-data_Vy = np.array(data_Vy)
-
-def plot_result(data_u:np.ndarray, data_Vx:np.ndarray, data_Vy:np.ndarray, time:int, picture:bool, showMe:bool):
+def plot_result(data:np.ndarray, data_Vx:np.ndarray, data_Vy:np.ndarray, time:int, picture:bool, showMe:bool):
     """
     Plotting of u(t,x,y) and V(t,x,y) at a specific point in time
     """
 
-    X = np.linspace(0,data_u[0].shape[0],data_u[0].shape[0])                                            # our grid
-    Y = np.linspace(0,data_u[0].shape[1],data_u[0].shape[1])
-    
+    # frequency for quivers
+    m = 5
+    # shapes
+    size_x = data[0].shape[0]
+    size_y = data[0].shape[1]
+
+    # grid
+    X = np.linspace(0,size_x,size_x)
+    Y = np.linspace(0,size_y,size_y)
+
+    # making plot
     fig, ax = plt.subplots()
-    line = plt.imshow(data_u[time], aspect = 'auto', cmap = 'Greys_r', extent = [0,100,0,100])          # u slice
-    plt.colorbar(line, ax=ax)                                                                   
-    q = ax.quiver(X[::10], Y[::10], data_Vx[time][::10, ::10], data_Vy[time][::10, ::10], color='red')  # gradient for V
+    # show data
+    line = plt.imshow(data[:size_x-1, :size_y-1], aspect = 'auto', cmap = 'turbo', extent = [0,size_x,size_y,0])
+    plt.colorbar(line, ax=ax)
+    # gradient for velocity
+    ax.quiver(X[::m]+1, Y[::m]+1, data_Vx[0].T[::m, ::m], data_Vy[0].T[::m, ::m], color='red')
 
     ax.set_xlabel('x, m')
     ax.set_ylabel('y, m')
-    ax.set_title('u(x,y,t) and V(x,y,t) at t = {t}'.format(t=0))
+    ax.set_title('u(x,y) and V(x,y)')
 
+    # show picture
     if showMe == True:
         plt.show()
 
+    # saving picture
     if picture == True:
-        plt.savefig(f"t={data_u.shape[0]}, max_x={X.shape[0]}, max_y={Y.shape[0]},.png")                # saving picture
+        plt.savefig(f"max_x={X.shape[0]}, max_y={Y.shape[0]},.png")
 
 
-
-
-def anim_result(data_u:np.ndarray, data_Vx:np.ndarray, data_Vy:np.ndarray, picture:bool, showMe:bool):
+def anim_result(data:np.ndarray, data_Vx:np.ndarray, data_Vy:np.ndarray, picture:bool, showMe:bool):
     """
     Make animation of u(t,x,y) and V(t,x,y) and save it to gif
     """
 
-
-    def animate(i):                                                                                     # animate function for animation.FuncAnimation
-        line.set_array(data_u[i])                                                                       # change "slice" of u cube
-        ax.set_title(f'u(x,y,t) and V(x,y,t) at t = {i}')                                               # also change quiver
-        q.set_UVC(data_Vx[i][::10, ::10], data_Vy[i][::10, ::10])       
+    # animate function for animation.FuncAnimation
+    def animate(i):
+        # change "slice" of data cube
+        line.set_array(data[i][:size_x-1, :size_y-1])
+        ax.set_title(f'u(x,y,t) and V(x,y,t) at t = {i}')
+        q = ax.quiver(X[::m]+1, Y[::m]+1, data_Vx[i].T[::m, ::m], data_Vy[i].T[::m, ::m], color='red')
         return line, q
     
 
-    X = np.linspace(0,data_u[0].shape[0],data_u[0].shape[0])                                            # our grid
-    Y = np.linspace(0,data_u[0].shape[1],data_u[0].shape[1])
+    # frequency for quivers
+    m = 5
+    # shapes
+    size_t = data.shape[0]
+    size_x = data[0].shape[0]
+    size_y = data[0].shape[1]
+
+    # grid
+    X = np.linspace(0,size_x,size_x)
+    Y = np.linspace(0,size_y,size_y)
     
+    # making first plot
     fig, ax = plt.subplots()
-    line = plt.imshow(data_u[0], aspect = 'auto', cmap = 'Greys_r', extent = [0,100,0,100])             # u slice
-    plt.colorbar(line, ax=ax)                                                                   
-    q = ax.quiver(X[::10], Y[::10], data_Vx[0][::10, ::10], data_Vy[0][::10, ::10], color='red')        # gradient for V
+    # first slice of data
+    line = plt.imshow(data[0][:size_x-1, :size_y-1], aspect = 'auto', cmap = 'turbo', extent = [0,size_x,size_y,0])
+    plt.colorbar(line, ax=ax)
+    plt.clim(np.min(data), np.max(data))
+    # gradient for velocity
+    ax.quiver(X[::m]+1, Y[::m]+1, data_Vx[0].T[::m, ::m], data_Vy[0].T[::m, ::m], color='red')
 
     ax.set_xlabel('x, m')
     ax.set_ylabel('y, m')
     ax.set_title('u(x,y,t) and V(x,y,t) at t = {t}'.format(t=0))
 
-    ani = animation.FuncAnimation(                                                                      # animate
-    fig, animate, interval=60, blit=True, frames = data_u.shape[0])
+    # animate
+    ani = animation.FuncAnimation(fig, animate, interval=30, blit=True, frames = size_t)
 
+    # show first frame
     if showMe == True:
         plt.show()
 
+    # save to .gif
     if picture == True:
-        writer = animation.PillowWriter(                                                                # saving picture
-            fps=30, metadata=dict(artist='Doofenshmirtz Evil Incorporated'), bitrate=1800)
-        ani.save(f"t={data_u.shape[0]}, max_x={X.shape[0]}, max_y={Y.shape[0]},.gif", writer=writer)
-
-
-# plot_result(data_u, data_Vx, data_Vy, time = 60, picture=True, showMe=True)
-
-anim_result(data_u, data_Vx, data_Vy, picture=True, showMe=True)
+        writer = animation.PillowWriter(
+            fps=120, metadata=dict(artist='Doofenshmirtz Evil Incorporated'), bitrate=1800)
+        ani.save(f"t={size_t}, max_x={size_x}, max_y={size_y},.gif", writer=writer)
