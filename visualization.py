480bdaed5373cc876ae6eb3c7659cbfeeddc27ab
Илья <149475177+Kusok0vody@users.noreply.github.com>
2024-02-07 21:30:01 +0700

    QoL

--- visualization.py
+++ visualization.py
@@ -1,12 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import os.path
 
 
-
-def plot_result(data:np.ndarray, data_Vx:np.ndarray, data_Vy:np.ndarray, time:int, picture:bool, showMe:bool):
+def plot_result(data,
+                data_Vx,
+                data_Vy,
+                path:str=None,
+                name:str=None,
+                colour:str='viridis',
+                save_picture:bool=False,
+                showMe:bool=False):
     """
-    Plotting of u(t,x,y) and V(t,x,y) at a specific point in time
+    Plotting of u(x,y) and V(x,y)
+    
+    Parameters
+    ----------
+    data : array_like
+        3d array of field data.
+    data_x : array_like
+        3d array of x-axis values for gradient.
+    data_y : array_like
+        3d array of x-axis values for gradient.
+    path : str, optional
+        Path where you want to save gif. If path==None, saves
+        to program's directory.
+    name : str, optional
+        Name for a picture, default name is given by shapes.
+    colour: str, optional
+        Colour for data field, colour by default is Viridis.
+    save_picture : bool, optional
+        If True, saves picture in .png format.
+    showMe : bool, optional
+        If True, show the plot.
     """
 
     # frequency for quivers
@@ -36,26 +63,93 @@ def plot_result(data:np.ndarray, data_Vx:np.ndarray, data_Vy:np.ndarray, time:in
         plt.show()
 
     # saving picture
-    if picture == True:
-        plt.savefig(f"max_x={X.shape[0]}, max_y={Y.shape[0]},.png")
+    if save_picture == True:
+        # plt.savefig(f"{path}/max_x={X.shape[0]}, max_y={Y.shape[0]},.png")
+        if name == None:
+            if path==None:
+                if os.path.exists(f"max_x={size_x}, max_y={size_y}.png")==True:
+                    k = 1
+                    while os.path.exists(f", max_x={size_x}, max_y={size_y}_{k}.png")==True:
+                        k += 1                    
+                    plt.savefig(f"max_x={size_x}, max_y={size_y}_{k}.png")
+                else:
+                    plt.savefig(f"max_x={size_x}, max_y={size_y}.png")
+            else:
+                if os.path.exists(f"{path}/max_x={size_x}, max_y={size_y}.png")==True:
+                    k = 1
+                    while os.path.exists(f"{path}/max_x={size_x}, max_y={size_y}_{k}.png")==True:
+                        k += 1                    
+                    plt.savefig(f"{path}/max_x={size_x}, max_y={size_y}_{k}.png")
+                else:
+                    plt.savefig(f"{path}/max_x={size_x}, max_y={size_y}.png")
+        else:
+            if path==None:
+                if os.path.exists(f"{name}.png")==True:
+                    k = 1
+                    while os.path.exists(f"{name}_{k}.png")==True:
+                        k += 1                    
+                    plt.savefig(f"{name}_{k}.png")
+                else:
+                    plt.savefig(f"{name}.png")
+            else:
+                if os.path.exists(f"{path}/{name}.png")==True:
+                    k = 1
+                    while os.path.exists(f"{path}/{name}_{k}.png")==True:
+                        k += 1                    
+                    plt.savefig(f"{path}/{name}_{k}.png")
+                else:
+                    plt.savefig(f"{path}/{name}.png")
 
-
-def anim_result(data:np.ndarray, data_Vx:np.ndarray, data_Vy:np.ndarray, picture:bool, showMe:bool):
+def anim_result(data,
+                data_x,
+                data_y,
+                path:str=None,
+                name:str=None,
+                colour:str='viridis',
+                grad_freq:int=5,
+                savetogif:bool=False,
+                showMe:bool=False,
+                ):
     """
-    Make animation of u(t,x,y) and V(t,x,y) and save it to gif
-    """
+    Make animation of u(t,x,y) and V(t,x,y) and, 
+    if necessary, save it to gif or show it
 
+    Parameters
+    ----------
+    data : array_like
+        3d array of field data for every frame.
+    data_x : array_like
+        3d array of x-axis values for gradient for every frame.
+    data_y : array_like
+        3d array of x-axis values for gradient for every frame.
+    path : str, optional
+        Path where you want to save gif. If path==None, saves
+        to program's directory.
+    name : str, optional
+        Name for a gif, default name is given by number of frames
+        and shapes.
+    colour: str, optional
+        Colour for data field, colour by default is Viridis.
+    grad_freq : int, optional
+        Gradient's arrows frequency, by default equals 5.
+    savetogif : bool, optional
+        If True, saves animation in .gif format.
+    showMe : bool, optional
+        If True, show the first frame of animation.
+    """
     # animate function for animation.FuncAnimation
     def animate(i):
         # change "slice" of data cube
         line.set_array(data[i][:size_x-1, :size_y-1])
         ax.set_title(f'u(x,y,t) and V(x,y,t) at t = {i}')
-        q = ax.quiver(X[::m]+1, Y[::m]+1, data_Vx[i].T[::m, ::m], data_Vy[i].T[::m, ::m], color='red')
+        q = ax.quiver(X[::grad_freq]+1,
+                      Y[::grad_freq]+1,
+                      data_x[i].T[::grad_freq, ::grad_freq],
+                      data_y[i].T[::grad_freq, ::grad_freq],
+                      color='red')
         return line, q
     
 
-    # frequency for quivers
-    m = 5
     # shapes
     size_t = data.shape[0]
     size_x = data[0].shape[0]
@@ -68,11 +162,15 @@ def anim_result(data:np.ndarray, data_Vx:np.ndarray, data_Vy:np.ndarray, picture
     # making first plot
     fig, ax = plt.subplots()
     # first slice of data
-    line = plt.imshow(data[0][:size_x-1, :size_y-1], aspect = 'auto', cmap = 'turbo', extent = [0,size_x,size_y,0])
+    line = plt.imshow(data[0][:size_x-1, :size_y-1], aspect = 'auto', cmap = colour, extent = [0,size_x,size_y,0])
     plt.colorbar(line, ax=ax)
     plt.clim(np.min(data), np.max(data))
     # gradient for velocity
-    ax.quiver(X[::m]+1, Y[::m]+1, data_Vx[0].T[::m, ::m], data_Vy[0].T[::m, ::m], color='red')
+    ax.quiver(X[::grad_freq]+1,
+              Y[::grad_freq]+1,
+              data_x[0].T[::grad_freq, ::grad_freq],
+              data_y[0].T[::grad_freq, ::grad_freq],
+              color='red')
 
     ax.set_xlabel('x, m')
     ax.set_ylabel('y, m')
@@ -85,8 +183,55 @@ def anim_result(data:np.ndarray, data_Vx:np.ndarray, data_Vy:np.ndarray, picture
     if showMe == True:
         plt.show()
 
-    # save to .gif
-    if picture == True:
-        writer = animation.PillowWriter(
-            fps=120, metadata=dict(artist='Doofenshmirtz Evil Incorporated'), bitrate=1800)
-        ani.save(f"t={size_t}, max_x={size_x}, max_y={size_y},.gif", writer=writer)
+    # save to .gif with all specifies
+    if savetogif == True:
+        if name == None:
+            if path==None:
+                if os.path.exists(f"t={size_t}, max_x={size_x}, max_y={size_y}.gif")==True:
+                    k = 1
+                    while os.path.exists(f"t={size_t}, max_x={size_x}, max_y={size_y}_{k}.gif")==True:
+                        k += 1                    
+                    writer = animation.PillowWriter(
+                        fps=120, metadata=dict(artist='Doofenshmirtz Evil Incorporated'), bitrate=1800)
+                    ani.save(f"t={size_t}, max_x={size_x}, max_y={size_y}_{k}.gif", writer=writer)
+                else:
+                    writer = animation.PillowWriter(
+                    fps=120, metadata=dict(artist='Doofenshmirtz Evil Incorporated'), bitrate=1800)
+                    ani.save(f"t={size_t}, max_x={size_x}, max_y={size_y}.gif", writer=writer)
+            else:
+                if os.path.exists(f"{path}/t={size_t}, max_x={size_x}, max_y={size_y}.gif")==True:
+                    k = 1
+                    while os.path.exists(f"{path}/t={size_t}, max_x={size_x}, max_y={size_y}_{k}.gif")==True:
+                        k += 1                    
+                    writer = animation.PillowWriter(
+                        fps=120, metadata=dict(artist='Doofenshmirtz Evil Incorporated'), bitrate=1800)
+                    ani.save(f"{path}/t={size_t}, max_x={size_x}, max_y={size_y}_{k}.gif", writer=writer)
+                else:
+                    writer = animation.PillowWriter(
+                    fps=120, metadata=dict(artist='Doofenshmirtz Evil Incorporated'), bitrate=1800)
+                    ani.save(f"{path}/t={size_t}, max_x={size_x}, max_y={size_y}.gif", writer=writer)
+        else:
+            if path==None:
+                if os.path.exists(f"{name}.gif")==True:
+                    k = 1
+                    while os.path.exists(f"{name}_{k}.gif")==True:
+                        k += 1                    
+                    writer = animation.PillowWriter(
+                        fps=120, metadata=dict(artist='Doofenshmirtz Evil Incorporated'), bitrate=1800)
+                    ani.save(f"{name}_{k}.gif", writer=writer)
+                else:
+                    writer = animation.PillowWriter(
+                    fps=120, metadata=dict(artist='Doofenshmirtz Evil Incorporated'), bitrate=1800)
+                    ani.save(f"{name}.gif", writer=writer)
+            else:
+                if os.path.exists(f"{path}/{name}.gif")==True:
+                    k = 1
+                    while os.path.exists(f"{path}/{name}_{k}.gif")==True:
+                        k += 1                    
+                    writer = animation.PillowWriter(
+                        fps=120, metadata=dict(artist='Doofenshmirtz Evil Incorporated'), bitrate=1800)
+                    ani.save(f"{path}/{name}_{k}.gif", writer=writer)
+                else:
+                    writer = animation.PillowWriter(
+                    fps=120, metadata=dict(artist='Doofenshmirtz Evil Incorporated'), bitrate=1800)
+                    ani.save(f"{path}/{name}.gif", writer=writer)
