98daafa1eaf2c46fcb5268814cfc3ac170f4b110
Илья <149475177+Kusok0vody@users.noreply.github.com>
2024-02-17 23:35:44 +0700

    Now it works

--- NSE_finite_difference.py
+++ NSE_finite_difference.py
@@ -8,6 +8,9 @@ def NSE_scheme(p_initial,
                kinematic_viscosity:float,
                density:float,
                frames_num:int,
+               dx:int,
+               dy:int,
+               dt:int,
                ):
     """
     Solver for NSE by finite difference method.
@@ -40,58 +43,78 @@ def NSE_scheme(p_initial,
     v : ndarray
         A 3d array of y-axis velocity for every frame.
     """
-
-    #shapes
+    # shapes
     size_x = p_initial.shape[0]
     size_y = p_initial.shape[1]
 
-    # grid step
-    dx = 0.5
-    dy = 0.5
-    # time step
-    dt = 0.01
-
-    #kinematic viscosity
+    # kinematic viscosity
     nu = kinematic_viscosity/density
 
-    #making arrays for every time step
+    # making arrays for every time step
     p = np.zeros((frames_num, size_x, size_y))
     p[0] += p_initial
     u = np.zeros((frames_num, size_x, size_y))
     u[0] += u_initial
     v = np.zeros((frames_num, size_x, size_y))
     v[0] += v_initial
-
+    
     # main loop
     for k in range(0, frames_num-1):
 
-        # first step: making Vx and Vy without p
+                # Dirichlet BC
+        u[:,0,:] = u[:,1,:]
+        u[:,-1,:] = u[:,-2,:]
+        u[:,:,0] = u[:,:,1]
+        u[:,:,-1] = u[:,:,-2]
+
+        v[:,0,:] = v[:,1,:]
+        v[:,-1,:] = v[:,-2,:]
+        v[:,:,0] = v[:,:,1]
+        v[:,:,-1] = v[:,:,-2]
+
+        # Dirichlet BC for pressure: equals IC
+        p[:,0,:] = p_initial[0,:]
+        p[:,-1,:] = p_initial[-1,:]
+        p[:,:,0] = p_initial[:,0]
+        p[:,:,-1] = p_initial[:,-1]
+
+        # Neumann BC
+        # p[:,0,:] = p[:,1,:]
+        # p[:,-1,:] = p[:,-2,:]
+        # p[:,:,0] = p[:,:,1]
+        # p[:,:,-1] = p[:,:,-2]
+        
+        # first step: making u and v without pressure
         tilda_u = np.zeros((size_x, size_y))
-        for i in range(0, size_x-1):
-            for j in range(0, size_y-1):
-                laplace_u = (u[k][i+1][j] - 2*u[k][i][j] + u[k][i-1][j]) / dx**2 + (u[k][i][j+1] - 2*u[k][i][j] + u[k][i][j-1]) / dy**2
-                nabla_u = u[k][i][j] * (u[k][i+1][j] - u[k][i][j]) / dx
+        for i in range(1, size_x-1):
+            for j in range(1, size_y-1):
+                laplace_u = (u[k][i][j+1] - 2*u[k][i][j] + u[k][i][j-1]) / dx**2 + (u[k][i+1][j] - 2*u[k][i][j] + u[k][i-1][j]) / dy**2
+                nabla_u = (2 * u[k][i][j] * (u[k][i][j+1] - u[k][i][j]) / dx 
+                + v[k][i][j] * (u[k][i+1][j] - u[k][i][j]) / dy 
+                + u[k][i][j] * (v[k][i+1][j] - v[k][i][j]) / dy)
                 tilda_u[i][j] = u[k][i][j] + dt * (Force_x[k][i][j] + nu*laplace_u - nabla_u)
 
         tilda_v = np.zeros((size_x, size_y))
-        for i in range(0, size_x-1):
-            for j in range(0, size_y-1):
-                laplace_v = (v[k][i+1][j] - 2*v[k][i][j] + v[k][i-1][j]) / dx**2 + (v[k][i][j+1] - 2*v[k][i][j] + v[k][i][j-1]) / dy**2
-                nabla_v = v[k][i][j] * (v[k][i][j+1] - v[k][i][j]) / dy
-                # 0 with nabla because nabla_v broke stability (why? I don't know...)
-                tilda_v[i][j] = v[k][i][j] + dt * (Force_y[k][i][j] + nu*laplace_v - 0*nabla_v)
+        for i in range(1, size_x-1):
+            for j in range(1, size_y-1):
+                laplace_v = (v[k][i][j+1] - 2*v[k][i][j] + v[k][i][j-1]) / dx**2 + (v[k][i+1][j] - 2*v[k][i][j] + v[k][i-1][j]) / dy**2
+                nabla_v = (2 * v[k][i][j] * (v[k][i+1][j] - v[k][i][j]) / dy 
+                + v[k][i][j] * (u[k][i][j+1] - u[k][i][j]) / dx 
+                + u[k][i][j] * (v[k][i][j+1] - v[k][i][j]) / dx)
+                tilda_v[i][j] = v[k][i][j] + dt * (Force_y[k][i][j] + nu*laplace_v - nabla_v)
 
-        # second step: calculating p (we should have div(p)=0)
-        for i in range(0, size_x-1):
-            for j in range(0, size_y-1):
-                laplace_p = (p[k][i+1][j] - 2*p[k][i][j] + p[k][i-1][j]) * dx**3 + (p[k][i][j-1] - 2*p[k][i][j] + p[k][i][j-1]) * dy**3
-                divergence = density * (dx * dy**2 * (tilda_u[i+1][j] - tilda_u[i][j]) + dx**2 * dy * (tilda_v[i][j+1] - tilda_v[i][j])) / dt    
-                p[k][i][j] = laplace_p - divergence + p[k][i][j]
+        # second step: calculating p (uses heat equation instead of Poisson assuming that pressure doesn't change)
+        for i in range(1, size_x-1):
+            for j in range(1, size_y-1):
+                laplace_p = (p[k][i][j+1] - 2*p[k][i][j] + p[k][i][j-1]) / dx**2 + (p[k][i+1][j] - 2*p[k][i][j] + p[k][i-1][j]) / dy**2
+                divergence = (tilda_u[i][j+1] - tilda_u[i][j]) / dx + (tilda_v[i+1][j] - tilda_v[i][j]) / dy
+                p[k+1][i][j] = dt / density * laplace_p - divergence + p[k][i][j]
 
         # third step: calculating true velocities with right pressure
-        for i in range(0, size_x-1):
-            for j in range(0, size_y-1):
-                u[k+1][i][j] = tilda_u[i][j] - dt * ((p[k][i][j] - p[k][i-1][j]) / dx) / density
-                v[k+1][i][j] = tilda_v[i][j] - dt * ((p[k][i][j] - p[k][i-1][j]) / dy) / density
+        for i in range(1, size_x-1):
+            for j in range(1, size_y-1):
+                u[k+1][i][j] = np.round(tilda_u[i][j] - dt * ((p[k+1][i][j+1] - p[k+1][i][j]) / dx) / density,5)
+                v[k+1][i][j] = np.round(tilda_v[i][j] - dt * ((p[k+1][i+1][j] - p[k+1][i][j]) / dy) / density,5)
         
     return p, u, v
+
